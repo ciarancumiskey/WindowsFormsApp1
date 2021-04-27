49437fe77deca70ddf8ac8a957ed619136aa1fe4
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-04-27 11:54:27 +0100

    Implemented UI and click functionality
    Added text fields with labels and a button for the user to click.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.textSurname = new System.Windows.Forms.TextBox();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Location = new System.Drawing.Point(91, 12);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(100, 23);
+            this.textFirstName.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(25, 15);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(62, 15);
+            this.labelFirstName.TabIndex = 2;
+            this.labelFirstName.Text = "First name";
+            // 
+            // textSurname
+            // 
+            this.textSurname.Location = new System.Drawing.Point(91, 41);
+            this.textSurname.Name = "textSurname";
+            this.textSurname.Size = new System.Drawing.Size(100, 23);
+            this.textSurname.TabIndex = 0;
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(25, 44);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(54, 15);
+            this.labelSurname.TabIndex = 2;
+            this.labelSurname.Text = "Surname";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(91, 71);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(100, 23);
+            this.textEmail.TabIndex = 3;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(25, 74);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(36, 15);
+            this.labelEmail.TabIndex = 2;
+            this.labelEmail.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.textSurname);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textFirstName);
+            this.Name = "Form1";
+            this.Text = "Hello there";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.TextBox textSurname;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,14 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Hello " + textFirstName.Text + "! Thank you for registering.";
+        }
     }
 }
